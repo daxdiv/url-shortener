d6c98b84ae8032f7bd22118bbcd7272a710a6dc2
daxdiv <95906740+daxdiv@users.noreply.github.com>
2022-11-02 14:30:35 +0100

    create a unique shortened url

--- src/pages/components/common/Input.tsx
+++ src/pages/components/common/Input.tsx
@@ -1,24 +1,70 @@
-import { useRef } from "react";
+import Link from "next/link";
 import Button from "./Button";
+import { useState } from "react";
+import { trpc } from "../../../utils/trpc";
+import { env } from "../../../env/client.mjs";
+
+interface IShortenedUrls {
+  shortenUrl: string;
+  aliasOf: string;
+}
 
 const Input = () => {
-  const inputRef = useRef<HTMLInputElement>(null);
+  const [url, setUrl] = useState("");
+  const [shortenedUrls, setShortenedUrls] = useState<IShortenedUrls[]>([]);
+  const shortenUrlMutation = trpc.url.shorten.useMutation();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (url === "") return;
+
+    shortenUrlMutation.mutate(
+      { url },
+      {
+        onSuccess: ({ shortenUrl, aliasOf }) => {
+          console.log(`created ${shortenUrl} - alias of ${aliasOf}`);
+
+          setShortenedUrls((prev) => [...prev, { shortenUrl, aliasOf }]);
+          setUrl("");
+        },
+      }
+    );
+  };
 
   return (
-    <form>
-      <input
-        type="text"
-        placeholder="Your link goes here"
-        className="mr-2 h-12 w-96 rounded-lg bg-gray-700 px-4 text-lg font-normal focus:outline-none focus:ring-2 focus:ring-blue-600"
-        value={inputRef.current?.value}
-        onChange={(e) => {
-          if (inputRef.current) {
-            inputRef.current.value = e.target.value;
-          }
-        }}
-      />
+    <form
+      className="flex flex-col items-center justify-center"
+      onSubmit={handleSubmit}
+    >
+      <div>
+        <input
+          type="text"
+          placeholder="Your URL goes here"
+          className="mr-2 h-12 w-96 rounded-lg bg-gray-700 px-4 text-lg font-normal focus:outline-none focus:ring-2 focus:ring-blue-600"
+          value={url}
+          onChange={(e) => {
+            setUrl(e.target.value);
+          }}
+        />
+        <Button text="Shorten" type="submit" />
+      </div>
 
-      <Button text="Shorten" type="submit" />
+      <ul className="mt-4 items-center justify-center font-normal">
+        {shortenedUrls.map(({ shortenUrl, aliasOf }) => (
+          <li key={`${Math.random()}-${shortenUrl}`}>
+            <Link
+              href={aliasOf}
+              target="_blank"
+              rel="noreferrer"
+              className="hover:text-blue-600"
+            >
+              {env.NEXT_PUBLIC_BASE_URL}/{shortenUrl}
+            </Link>
+            <span> - alias of {aliasOf}</span>
+          </li>
+        ))}
+      </ul>
     </form>
   );
 };
