bda43caeb69d15cbc928ceb5aba63140e02fc66f
pollettoimpanato <david200288l@gmail.com>
2022-11-08 11:18:03 +0100

    use http-status-codes instead of code numbers

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,20 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { StatusCodes as HTTPStatusCodes } from "http-status-codes";
 
 export async function middleware(req: NextRequest) {
   const shortenUrl = req.nextUrl.pathname.split("/")[1]; // NOTE: example pathname is /[url]
   const res = await fetch(`${req.nextUrl.origin}/api/url/${shortenUrl}`);
 
-  if (res.status === 404) {
+  if (res.status === HTTPStatusCodes.BAD_REQUEST) {
     return NextResponse.redirect(req.nextUrl.origin);
   }
 
   const data = await res.json();
 
-  return NextResponse.redirect(data.aliasOf);
+  return NextResponse.redirect(data.aliasOf, {
+    status: HTTPStatusCodes.PERMANENT_REDIRECT,
+  });
 }
 
 export const config = {
