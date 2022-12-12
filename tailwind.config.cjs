e74723bceeb1ff87d914cc1890cb9f4b7a94e18f
daxdiv <95906740+daxdiv@users.noreply.github.com>
2022-12-12 14:28:18 +0100

    dialog fade in/out animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,9 +10,19 @@ module.exports = {
           "30%, 50%, 70%": { transform: "translate3d(-4px, 0, 0)" },
           "40%, 60%": { transform: "translate3d(4px, 0, 0)" },
         },
+        "dialog-in": {
+          "0%": { opacity: 0, transform: "scale(0.9)" },
+          "100%": { opacity: 1, transform: "scale(1)" },
+        },
+        "dialog-out": {
+          "0%": { opacity: 1, transform: "scale(1)" },
+          "100%": { opacity: 0, transform: "scale(0.9)" },
+        },
       },
       animation: {
         shaking: "shake 0.82s cubic-bezier(.36,.07,.19,.97) both",
+        "dialog-in": "dialog-in 0.3s ease-out forwards",
+        "dialog-out": "dialog-out 0.3s ease-out forwards",
       },
     },
   },
