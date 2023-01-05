469a3e3caa465ab5f732af94cf8a0c81e3ac7152
daxdiv <95906740+daxdiv@users.noreply.github.com>
2023-01-05 19:23:59 +0100

    formatDate helper function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -13,3 +13,14 @@ export const isValidUrl = (s: string) => {
 
   return url.protocol === "http:" || url.protocol === "https:";
 };
+
+export const formatDate = (date: Date) => {
+  const locale = navigator.language;
+  const formatter = new Intl.DateTimeFormat(locale, {
+    year: "numeric",
+    month: "short",
+    day: "2-digit",
+  });
+
+  return formatter.format(date);
+};
